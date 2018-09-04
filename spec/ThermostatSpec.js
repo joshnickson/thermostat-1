f8b64a212e06e700f2f026932f10fd07016ad650
davidlawes <davidlawes@hotmail.co.uk>
2018-09-04 16:39:28 +0100

    Included up function in Thermostat

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,6 +7,13 @@ describe('Thermostat', function() {
       thermostat = new Thermostat;
     })
     it('has an initial setting of 20 degrees', function() {
-      expect(thermostat.temperature()).toEqual(20);
+      expect(thermostat.temperature()).toEqual(thermostat.DEFAULT_TEMPERATURE);
+    });
+
+    describe('Thermostat temperature moderation', function() {
+      it('Thermostat.prototype.up increases temperature by 1', function() {
+        thermostat.up();
+        expect(thermostat.temperature()).toEqual(thermostat.DEFAULT_TEMPERATURE + 1)
+      });
     });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -6,3 +6,7 @@ function Thermostat() {
 Thermostat.prototype.temperature = function() {
   return this._temperature;
 };
+
+Thermostat.prototype.up = function() {
+  this._temperature++; // we know this returns undefined 
+};
